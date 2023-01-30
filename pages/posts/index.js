81b93a7331c20ee0745f724609c7fa06be4a35e7
icarvar <ivo.cardozo@preservica.com>
2023-01-30 15:55:48 -0400

    Add the 'All Posts' page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,43 @@
 import React from 'react'
 
+import AllPosts from '@/components/posts/all-posts'
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    },
+    {
+        slug: 'getting-started-with-nextjs2',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    },{
+        slug: 'getting-started-with-nextjs3',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    },
+    {
+        slug: 'getting-started-with-nextjs4',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    }
+]
+
 const AllPostsPage = () => {
- 
+    return <AllPosts posts={DUMMY_POSTS}/>
 }
 
 export default AllPostsPage
