52eb0ab7dbc7ace9caba7d54b922a4faf50552ce
icarvar <ivo.cardozo@preservica.com>
2023-01-30 16:22:44 -0400

    Working on the 'Post detail' page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PostHeader from './post-header'
+
+import classes from './post-content.module.css'
+
+const DUMMY_POST = {
+    slug: 'getting-started-with-nextjs',
+    title: 'getting started with nextJs',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+        'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+    date: '2022-02-10',
+    content: '# This is a first post'
+}
+
+const PostContent = () => {
+    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+  return (
+    <article className={classes.content}>
+        <PostHeader title={DUMMY_POST.title} image={imagePath}/>
+        {DUMMY_POST.content}
+    </article>
+  )
+}
+
+export default PostContent
