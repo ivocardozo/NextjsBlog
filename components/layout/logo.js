024317ec1eca0db9a4496ca3b6887eb7b60eabef
icarvar <ivo.cardozo@preservica.com>
2023-01-30 14:32:36 -0400

    add styling and logo

---
+++ components/layout/logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import classes from './Logo.module.css'
+
+const Logo = () => {
+  return (
+    <div className={classes.logo}>
+      Max' Next Blog
+    </div>
+  )
+}
+
+export default Logo
