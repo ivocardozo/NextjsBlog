832cb5a4f9cfed4dd697d8dbcd10ecd1d7750f42
icarvar <ivo.cardozo@preservica.com>
2023-01-31 11:35:45 -0400

    rendering images with next imgaes component

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,15 +3,49 @@ import React from 'react'
 import PostHeader from './post-header'
 
 import classes from './post-content.module.css'
+import Image from 'next/image'
 
 const PostContent = (props) => {
   const { post } = props
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`
+
+  const customRenderers = {
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       widt={600}
+    //       height={300}
+    //     />
+    //   )
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph
+
+      if (node.children[0].type === 'image') {
+        const image = node.children[0]
+
+        return (
+          <div>
+            <Image
+            src={`/images/posts/${post.slug}/${image.url}`}
+            alt={image.alt}
+            widt={600}
+            height={300}
+            />
+          </div>
+        )
+      }
+
+      return <p>{paragraph.children}</p>
+    }
+  }
   return (
     <article className={classes.content}>
         <PostHeader title={post.title} image={imagePath}/>
-        <ReactMarkdown>{post.content}</ReactMarkdown>
+        <ReactMarkdown renderers={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   )
 }
