6a30ccd753e1b5ae9f235789c0488d703d759ac1
icarvar <ivo.cardozo@preservica.com>
2023-01-31 14:30:27 -0400

    Adding 'head' Data

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Head from 'next/head'
 
 import PostContent from '@/components/posts/post-detail/post-content'
 import { getPostData, getPostsFiles } from '@/lib/posts-util'
 
 const PostDetailPage = (props) => {
-    return <PostContent post={props.post}/>
+    return (
+        <Fragment>
+            <Head>
+                <title>{props.post.title}</title>
+                <meta 
+                    name='description'
+                    content={props.post.excerpt}
+                />
+            </Head>
+            <PostContent post={props.post}/>
+        </Fragment>
+    )
 }
 
 export function getStaticProps(context) {
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import AllPosts from '@/components/posts/all-posts'
 
 import { getAllPosts } from '../../lib/posts-util';
+import Head from 'next/head';
 
 const AllPostsPage = (props) => {
-    return <AllPosts posts={props.posts}/>
+    return (
+        <Fragment>
+            <Head>
+                <title>All Posts</title>
+                <meta
+                    name='description'
+                    content='A list of all programming-related tutorials and posts!'
+                />
+            </Head>
+            <AllPosts posts={props.posts}/>
+        </Fragment>
+    )
 }
 export function getStaticProps() {
     const allPosts = getAllPosts();
