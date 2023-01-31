deff45e6a6b33ca3ae73ff0296314c15b3a7bb24
icarvar <ivo.cardozo@preservica.com>
2023-01-31 10:10:32 -0400

    Rendering dynamic post pages & paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,22 +4,14 @@ import PostHeader from './post-header'
 
 import classes from './post-content.module.css'
 
-const DUMMY_POST = {
-    slug: 'getting-started-with-nextjs',
-    title: 'getting started with nextJs',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-        'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
-    date: '2022-02-10',
-    content: '# This is a first post'
-}
+const PostContent = (props) => {
+  const { post } = props
 
-const PostContent = () => {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+  const imagePath = `/images/posts/${post.slug}/${post.image}`
   return (
     <article className={classes.content}>
-        <PostHeader title={DUMMY_POST.title} image={imagePath}/>
-        <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+        <PostHeader title={post.title} image={imagePath}/>
+        <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   )
 }
