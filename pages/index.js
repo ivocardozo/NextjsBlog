7983cd3e50f0c7684c9c73525788d698984a9253
icarvar <ivo.cardozo@preservica.com>
2023-02-01 09:51:12 -0400

    Using markdown data for rendering posts on home page

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,24 @@
 import React, { Fragment } from 'react'
+
 import FeaturedPosts from '@/components/home-page/featured-posts'
-
-const DUMMY_POSTS = [
-  {
-      slug: 'getting-started-with-nextjs',
-      title: 'getting started with nextJs',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-          'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
-      date: '2022-02-10'
-  },
-  {
-      slug: 'getting-started-with-nextjs2',
-      title: 'getting started with nextJs',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-          'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
-      date: '2022-02-10'
-  },{
-      slug: 'getting-started-with-nextjs3',
-      title: 'getting started with nextJs',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-          'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
-      date: '2022-02-10'
-  },
-  {
-      slug: 'getting-started-with-nextjs4',
-      title: 'getting started with nextJs',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-          'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
-      date: '2022-02-10'
-  }
-]
-
 import Hero from '@/components/home-page/hero'
-const HomePage = () => {
+import { getFeaturedPosts } from '../lib/posts-util'
+
+const HomePage = (props) => {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts posts={DUMMY_POSTS}/>
+      <FeaturedPosts posts={props.posts}/>
     </Fragment>
   )
 }
 
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts()
+  return {
+    props: {
+      posts: featuredPosts
+    }
+  }
+}
 export default HomePage
