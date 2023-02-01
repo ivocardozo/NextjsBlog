565a5ef519733c718e8ae81719d425e1f36a7db8
icarvar <ivo.cardozo@preservica.com>
2023-02-01 11:00:28 -0400

    rendering image with markdwon inside the paragraph

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -9,16 +9,36 @@ const PostContent = (props) => {
   const { post } = props
   const imagePath = `/images/posts/${post.slug}/${post.image}`
   const customRenderers = {
-    image(image) {
-      return (
-        <Image
-          src={`/images/posts/${post.slug}/${image.src}`}
-          alt={image.alt}
-          width={600}
-          height={300}
-        />
-      );
-    },
+    // image(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    paragraph(paragraph) {
+      const { node } = paragraph
+
+      if ( node.children[0].type === 'image') {
+        const image = node.children[0]
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.url}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        )
+      }
+
+      return <p>{paragraph.children}</p>
+    }
   }
   return (
     <article className={classes.content}>
