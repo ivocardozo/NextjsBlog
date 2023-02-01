1813ff05239056567faabde991fd5a68fe14fb9c
icarvar <ivo.cardozo@preservica.com>
2023-02-01 14:18:22 -0400

    Adding 'head' data

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Head from 'next/head'
 
 import PostContent from '@/components/posts/post-detail/post-content'
 import { getPostData, getPostsFiles } from '@/lib/posts-util'
 const PostDetailpage = (props) => {
   return (
-    <PostContent post={props.post}/>
+    <Fragment>
+      <Head>
+        <title>{props.post.title}</title>
+        <meta 
+          name='description'
+          content={props.post.excerpt}
+        />
+      </Head>
+      <PostContent post={props.post}/>
+    </Fragment>
   )
 }
 
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import Head from 'next/head'
 
 import AllPosts from '@/components/posts/all-posts'
 import { getAllPosts } from '@/lib/posts-util'
 
 const AllPostsPage = (props) => {
   return (
-    <AllPosts posts={props.posts}/>
+    <Fragment>
+        <Head>
+            <title>All posts</title>
+            <meta 
+                name='description'
+                content='A list of all programming-related tutorial and posts'
+            />
+        </Head>
+        <AllPosts posts={props.posts}/>
+    </Fragment>
   )
 }
 
