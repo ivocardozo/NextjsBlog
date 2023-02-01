edf18c0ffcffd1ea0f2b4c54f7cf86f218500765
icarvar <ivo.cardozo@preservica.com>
2023-02-01 11:57:28 -0400

    Storing messages with mongo db database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler (req, res) {
+import { MongoClient } from 'mongodb'
+
+async function handler (req, res) {
     if(req.method === 'POST') {
         const { email, name, message } = req.body
 
@@ -13,14 +15,30 @@ function handler (req, res) {
             res.status(422).json({message: 'Invalid input'})
             return
         }
-        //Store it in the database
         const newMessage = {
             email,
             name, 
             message
         }
+        let client
+        try {
+            client = await MongoClient.connect('mongodb+srv://user:ACEpybfIeOQl0G7h@cluster0.7zojtyj.mongodb.net/my-site')
+        } catch(error) {
+            res.status(500).json({message:'Could not connect to database.'})
+            return
+        }
+        const db = client.db()
+        try {
+            const result = await db.collection('messages').insertOne(newMessage)
+            newMessage.id = result.insertedId
+        } catch(error) {
+            client.close()
+            res.status(500).json({message: 'Storing message failed!'})
+            return
+        }
 
-        console.log(newMessage)
+        client.close()
+
         res
             .status(201)
             .json({message: 'Successfully stored message!', message: newMessage})
