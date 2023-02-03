b8fc259361a84e2f83bbf32028e3abe88f7b8251
icarvar <ivo.cardozo@preservica.com>
2023-02-03 17:44:17 -0400

    rendering dummy data

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import PostsGrid from '../posts/posts-grid'
+import classes from './featured-posts.module.css'
+
+const FeaturedPosts = (props) => {
+  return (
+    <section>
+      <h2>Featured Posts</h2>
+      <PostsGrid posts={props.posts}/>
+    </section>
+  )
+}
+
+export default FeaturedPosts
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import Hero from '@/components/home-page/hero'
+import FeaturedPosts from '@/components/home-page/featured-posts'
+
+const DUMMY_POSTS = [
+    {
+        slug: 'getting-started-with-nextjs',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    },
+    {
+        slug: 'getting-started-with-nextjs2',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    },{
+        slug: 'getting-started-with-nextjs3',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    },
+    {
+        slug: 'getting-started-with-nextjs4',
+        title: 'getting started with nextJs',
+        image: 'getting-started-nextjs.png',
+        excerpt:
+            'NextJs is a the React framwork for production - it makes building fullstack React apps and sites a breeze and ships with built-in SSR',
+        date: '2022-02-10'
+    }
+  ]
 
 const HomePage = () => {
   return (
-    <Hero />
+    <Fragment>
+        <Hero />
+        <FeaturedPosts posts={DUMMY_POSTS}/>
+    </Fragment>
   )
 }
 
