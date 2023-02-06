11af09f24d96dc0b7d78f3d54bebd797b4b641fb
icarvar <ivo.cardozo@preservica.com>
2023-02-06 10:14:52 -0400

    Rendering dynamic post pages & paths

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,11 +1,38 @@
 import React from 'react'
 
 import PostContent from '@/components/posts/post-detail/post-content'
+import { getPostData, getPostsFiles } from '@/lib/posts-util'
 
-const PostDetailPage = () => {
+
+const PostDetailPage = (props) => {
   return (
-    <PostContent />
+    <PostContent posts={props.post}/>
   )
 }
 
 export default PostDetailPage
+
+export function getStaticProps(context) {
+  const{ params } = context
+  const { slug } = params
+
+  const postData = getPostData(slug)
+
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600
+  }
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles()
+
+  const slugs = postFileNames.map(fileName => fileName.replace(/\.md$/, ''))
+
+    return { 
+    paths: slugs.map(slug => ({params: { slug: slug}})),
+    fallback: false
+  }
+}
