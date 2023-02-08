f22b9fd793f4cb123e6ad37e4a7fce2f2493fd50
icarvar <ivo.cardozo@preservica.com>
2023-02-08 10:23:21 -0400

    Rendering dynamic post pages & paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,25 +4,17 @@ import ReactMarkdown from 'react-markdown'
 import PostHeader from './post-header'
 import classes from './post-content.module.css'
 
-const DUMMY_POST = {
-    slug: 'getting-started-with-nextjs',
-    title: 'getting started with nextJs',
-    image: 'getting-started-nextjs.png',
-    excerpt: '# This is a first post',
-    date: '2022-02-10',
-    content: '# This is a first post'
-}
-
-const PostContent = () => {
-    const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+const PostContent = (props) => {
+  const { posts } = props
+    const imagePath = `/images/posts/${posts.slug}/${posts.image}`
   return (
     <article className={classes.content}>
       <PostHeader 
-        title={DUMMY_POST.title}
+        title={posts.title}
         image={imagePath}
         />
       <ReactMarkdown>
-        {DUMMY_POST.content}
+        {posts.content}
       </ReactMarkdown>
     </article>
   )
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,11 +1,37 @@
 import React from 'react'
 
+import { getPostsFiles, getPostData } from '@/lib/posts-util'
+
 import PostContent from '@/components/posts/post-detail/post-content'
 
-const PostDetailPage = () => {
+const PostDetailPage = (props) => {
   return (
-    <PostContent />
+    <PostContent posts={props.post}/>
   )
+}
+
+export function getStaticProps(context) {
+  const { params } = context
+  const { slug } = params
+
+  const postData = getPostData(slug)
+
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600
+  }
+}
+
+export function getStaticPaths() {
+  const postFileNames = getPostsFiles()
+
+  const slugs = postFileNames.map(fileName => fileName.replace(/\.md$/, ''))
+  return {
+    paths: slugs.map(slug => ({params: {slug: slug}})),
+    fallback: false
+  }
 }
 
 export default PostDetailPage
